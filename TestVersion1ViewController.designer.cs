23c1bfc2fa10495522bd0e5d7e95019c848435db
unknown-user <unknown-user@192.168.3.46>
2014-10-14 21:28:57 +0530

    Initial check-in of module TestVersion1

---
+++ TestVersion1ViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace TestVersion1
+{
+	[Register ("TestVersion1ViewController")]
+	partial class TestVersion1ViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
